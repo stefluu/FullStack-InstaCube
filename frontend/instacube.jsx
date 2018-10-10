6725399c2f0540ceec216db5b0de2382b2f81e68
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-09 23:06:03 -0700

    Complete some Application Controller, complete webpack config, create db

---
+++ frontend/instacube.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configureStore } from './store/store'
+
+document.addEventListener("DOMContentLoaded", () =>{
+  // const store = configureStore();
+  const root = document.getElementById("root");
+
+  ReactDOM.render(
+    <h1>Instagram Clone!</h1>,
+    root
+  );
+});

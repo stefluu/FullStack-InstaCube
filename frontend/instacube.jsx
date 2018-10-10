33149f855b7dc1ddbec4700ace6123f5b26d8ed2
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-10 16:46:58 -0700

    Prepare project for production/Heroku setup

--- frontend/instacube.jsx
+++ frontend/instacube.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout }
+import { signup, login, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () =>{
   const root = document.getElementById('root');
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 
   ReactDOM.render(
     <h1>Instagram Clone!</h1>,
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,29 +1,23 @@
 
 export const signup = (user) => {
-  return{
-    $.ajax({
-      url: 'api/users',
-      method: 'POST',
-      data: { user }
-    })
-  };
+  return $.ajax({
+    url: 'api/users',
+    method: 'POST',
+    data: { user }
+  });
 };
 
 export const login = (user) =>{
-  return{
-    $.ajax({
-      url: 'api/session',
-      method: 'POST',
-      data: { user }
-    })
-  };
+  return $.ajax({
+    url: 'api/session',
+    method: 'POST',
+    data: { user }
+  });
 };
 
 export const logout = () => {
-  return{
-    $.ajax({
-      url: 'api/session',
-      method: 'DELETE'
-    })
-  }
-}
+  $.ajax({
+    url: 'api/session',
+    method: 'DELETE'
+  });
+};

403ff1a60bd3f9713b1aca7462eae81001d8a2b3
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-11 14:43:06 -0700

    Complete AuthRoute for User Auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Route, HashRouter, Link } from 'react-router-dom';
 // import LoginContainer from './session/login_container';
 import SignupContainer from './session/signup_container';
-import Splash from './splash/splash'
-import IndexContainer from './images/index'
+import Splash from './splash/splash';
+import IndexContainer from './images/index';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <h1>This is being rendered from app.jsx</h1>
     <Route exact path="/" component={Splash} />
-    <Route path="/signup" component={SignupContainer} />
-    <Route path="/index" component={IndexContainer} />
+    <AuthRoute path="/signup" component={SignupContainer} />
+    <ProtectedRoute path="/index" component={IndexContainer} />
   </div>
 );
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import LoginContainer from '../session/login_container';
+import AuthRoute from '../../util/route_util';
 
 
 class Splash extends React.Component{
   render(){
     return(
       <div>
-        <Route exact path="/" component={LoginContainer} />
-        <footer>
-          <ul id="footer-list">
-            <li>ABOUT US</li>
-            <li>SUPPORT</li>
-            <li>PRESS</li>
-            <li>API</li>
-            <li>JOBS</li>
-            <li>PRIVACY</li>
-            <li>TERMS</li>
-            <li>DIRECTORY</li>
-            <li>PROFILES</li>
-            <li>HASHTAG</li>
-            <li>LANGUAGE</li>
-          </ul>
-          <h4>© 2018 INSTACUBE</h4>
-        </footer>
+        <div id="splash">
+          <AuthRoute exact path="/" component={LoginContainer} />
+          <footer id="footer">
+            <ul id="footer-list">
+              <li>ABOUT US</li>
+              <li>SUPPORT</li>
+              <li>PRESS</li>
+              <li>API</li>
+              <li>JOBS</li>
+              <li>PRIVACY</li>
+              <li>TERMS</li>
+              <li>DIRECTORY</li>
+              <li>PROFILES</li>
+              <li>HASHTAG</li>
+              <li>LANGUAGE</li>
+            </ul>
+            <h4>© 2018 INSTACUBE</h4>
+          </footer>
+        </div>
       </div>
     )
   }

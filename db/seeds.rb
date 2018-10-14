5b498351aef7cc2572866ebec4e9ed0e05ddbbc8
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-13 19:08:07 -0700

    Seed data with users and photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,36 +8,53 @@
 
 dummyUser = User.create(username:"demo_user", full_name: "Stef L", email: "stef@testing.com", password: "123456")
 
-user1 = User.create(username: "CS.Photography", full_name: "Christopher", email: "chris@testing.com", password: "123456")
+user1 = User.create(username: "CS.Photography", full_name: "Christopher", display_name: "Photographer in Oakland", bio: "Film and Digital photography", email: "chris@testing.com", password: "123456")
 
-user2 = User.create(username: "NatureIsBeautiful", full_name: "Nature", email: "nature@testing.com", password: "123456")
+user2 = User.create(username: "NatureIsBeautiful", full_name: "Nature", bio: "Go Exploring!", email: "nature@testing.com", password: "123456")
 
-image1 = Image.create!(user_id: 48, date: Date.parse("2017-11-16"))
-# image1.save!
+user3 = User.create(username: "hey_itsStef", full_name: "Stefanie", display_name: "Stefanie.", bio: "Hi there! Welcome to my page!", email: "stef@stef.com", password: "123456")
 
-image2 = Image.create!(user_id: 48, date: Date.parse("2018-2-20"))
-# image2.save!
+image1 = Image.create!(user_id: 55, date: Date.parse("2017-11-16"), caption: "Hi, I'm Tagi!")
+# tagi
 
-image3 = Image.create!(user_id: 48, date: Date.parse("2018-8-12"))
-# image3.save!
+image2 = Image.create!(user_id: 55, date: Date.parse("2018-2-20"))
+# black and white
 
-image4 = Image.create!(user_id: 49, date: Date.parse("2018-1-23"))
-# image4.save!
+image3 = Image.create!(user_id: 55, date: Date.parse("2018-8-12"), caption: "This year's bloom.")
+# flower
 
-image5 = Image.create!(user_id: 49, date: Date.parse("2018-4-19"))
-# image5.save!
+image4 = Image.create!(user_id: 56, date: Date.parse("2018-1-23"), caption: "Light Blue")
+# beetle
 
-image6 = Image.create!(user_id: 49, date: Date.parse("2018-5-10"))
-# image6.save!
+image5 = Image.create!(user_id: 56, date: Date.parse("2018-4-19"), caption: "Lavender")
+# purplecup
 
-image7 = Image.create!(user_id: 49, date: Date.parse("2018-8-3"))
-# image7.save!
+image6 = Image.create!(user_id: 56, date: Date.parse("2018-5-10"))
+# leaf
 
-image8 = Image.create!(user_id: 50, date: Date.parse("2017-10-27"))
-# image8.save!
+image7 = Image.create!(user_id: 56, date: Date.parse("2018-8-3"))
+# chris
 
-image9 = Image.create!(user_id: 50, date: Date.parse("2018-2-2"))
-# image9.save!
+image8 = Image.create!(user_id: 56, date: Date.parse("2017-10-27"))
+# fooddrizzle
 
-image10 = Image.create!(user_id: 50, date: Date.parse("2018-7-16"))
-# image10.save!
+image9 = Image.create!(user_id: 56, date: Date.parse("2018-2-2"), caption: "Deviled Eggs")
+# deviledeggs
+
+image10 = Image.create!(user_id: 57, date: Date.parse("2018-7-16"), caption: "I love the beach.")
+# beach
+
+image11 = Image.create!(user_id: 57, date: Date.parse("2018-4-16"), caption: "Nature's paintings")
+# sunset
+
+image12 = Image.create!(user_id: 57, date: Date.parse("2017-9-20"), caption: "It's becoming fall!")
+#trees
+
+image13 = Image.create!(user_id: 58, date: Date.parse("2018-3-15"))
+#sparklers
+
+image14 = Image.create!(user_id: 58, date: Date.parse("2018-8-28"))
+# dandelion
+
+image15 = Image.create!(user_id: 58, date: Date.parse("2018-9-10"), caption: "Read more!")
+# grassreading
